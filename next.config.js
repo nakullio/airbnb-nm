e227f00bb7212fdd48af17fe87668f2f1bb29a08
Nakula <nakulamarvellio@gmail.com>
2021-11-21 00:07:24 +0700

    day one build

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // pass in the object on image
+    images: {
+        // set the domain that picture could accept from
+        domains: ['links.papareact.com'],
+
+    }
+}
